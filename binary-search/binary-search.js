e409c4a6b37fa33a413334516c20e0914a943007
yr.min17@gmail.com <Lily@aggieui-macbook.home>
2020-04-08 16:23:36 -0400

    bs complete

--- binary-search/binary-search.js
+++ binary-search/binary-search.js
@@ -1,8 +1,19 @@
 'use strict';
 
 // Complete this algo
-const binarySearch = (array, target) => {
-	
+const binarySearch = (array, target) => { // [1,2,3,4,5,6] 1 // [5, 10] 5
+	let start = 0; 
+	let end = array.length - 1; 
+	while (start <= end) {
+		let mid = Math.floor((start+end) / 2) // mid : 15, idx: 5 // 4 v: 10
+		if (array[mid] === target) return true;
+		else if (array[mid] < target) {  // 5 < 10
+			start = mid + 1 // 4
+		} else {
+			end = mid - 1 // 4
+		}
+	}
+	return false;
 };
 
 /*
